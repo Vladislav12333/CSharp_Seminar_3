d1fef59d25794f2711a28341fcac921e2183cf62
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-25 01:48:26 +0300

    Написали программу (Task_18) 2 вариант решения МАССИВОМ СТРОК

--- Task_18/Program.cs
+++ Task_18/Program.cs
@@ -1,23 +1,13 @@
 ﻿// Задача №17. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
-
+// 2 Вариант решения МАССИВОМ СТРОК
 System.Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+// строка с индексом (      0      ) (       1       ) (       2       ) (       3        )
+string[] array = { "X > 0 && Y > 0", "X < 0 && Y > 0", "X < 0 && Y < 0", "X > 0 && Y < 0" };
 
-if (number == 1)
+if (number > 0 && number < 5)
 {
-    System.Console.WriteLine("X > 0 && Y > 0");
-}
-else if (number == 2)
-{
-    System.Console.WriteLine("X < 0 && Y > 0");
-}
-else if (number == 3)
-{
-    System.Console.WriteLine("X < 0 && Y < 0");
-}
-else if (number == 4)
-{
-    System.Console.WriteLine("X > 0 && Y < 0");
+    System.Console.WriteLine(array[number - 1]);
 }
 else
 {
