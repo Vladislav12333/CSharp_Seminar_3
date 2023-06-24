6b27002096b970d25af4e8c465e5cbb8b7fe416b
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-25 01:29:18 +0300

    Написали программу (Task_18) 1 вариант решения задачи

---
+++ Task_18/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №17. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    System.Console.WriteLine("X > 0 && Y > 0");
+}
+else if (number == 2)
+{
+    System.Console.WriteLine("X < 0 && Y > 0");
+}
+else if (number == 3)
+{
+    System.Console.WriteLine("X < 0 && Y < 0");
+}
+else if (number == 4)
+{
+    System.Console.WriteLine("X > 0 && Y < 0");
+}
+else
+{
+    System.Console.WriteLine("Такой четверти не существует");
+}
