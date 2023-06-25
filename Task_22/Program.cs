f9fa5446a5692060e2ee925586256558d8bcc50d
Vladislav_Tishkovskij <vladislav.sergeevich.666@mail.ru>
2023-06-25 21:59:59 +0300

    Написали программу (Task_22) 3 вариант решения задачи

--- Task_22/Program.cs
+++ Task_22/Program.cs
@@ -1,12 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 // 5 -> 1, 4, 9, 16, 25.
 // 2 -> 1,4
-// Решения задачи с помощью цикла for
+// 3 Вариант решения задачи
 
-System.Console.WriteLine("Введите число: ");
+System.Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
-for(int i = 1; i <= number; i++)
+int[] array = new int[number];
+for (int i = 0; i < number; i++)
 {
-    System.Console.Write(Math.Pow(i, 2) + " ");
+    array[i] = (int)Math.Pow(i + 1, 2);
 }
+
+System.Console.WriteLine("[" + string.Join(", ", array) + "]");
